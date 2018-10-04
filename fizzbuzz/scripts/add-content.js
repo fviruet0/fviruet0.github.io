44180953c7f64998ac30396f5e6c9fa3f17d3c26
fviruet0 <fviruet0@email.cpcc.edu>
2018-10-04 19:57:39 -0400

    Brand Home Page
    This is my brand QuickFit.

---
+++ fizzbuzz/scripts/add-content.js
@@ -0,0 +1,13 @@
+﻿var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+	greeting = 'Good evening welcome to QuickFit!';
+} else if (hourNow > 12) {
+	greeting = 'Good afternoon welcome to QuickFit!';
+} else if (hourNow > 0) {
+	greeting = 'Welcome to QuickFit!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
